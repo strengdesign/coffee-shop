52f36da2f47e8ceb843eac40a13c0824b06a224e
derrikengel <derrik@derrikengel.com>
2015-02-26 16:30:22 -0600

    set up umbraco and basic site functionality

---
+++ website/scripts/scripts.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+
+	/* ==========================================================================
+	   variables
+	   ========================================================================== */
+	var navContainer = $('.site-nav');
+	var mainNav = $('.main-nav');
+
+	/* ==========================================================================
+	   mobile navigation
+	   ========================================================================== */
+	navContainer.prepend('<span class="nav-icon" title="Menu">&nbsp;<span class="icon"></span></span>');
+	mainNav.find('.menu').before('<span class="subnav-icon" title="Toggle">&nbsp;</span>');
+
+	$('.nav-icon').click(function () {
+		mainNav.toggleClass('active');
+		$(this).toggleClass('active');
+	});
+
+	mainNav.find('.subnav-icon').click(function () {
+		$(this).next('ul').slideToggle(250);
+		$(this).toggleClass('active');
+	});
+
+});
